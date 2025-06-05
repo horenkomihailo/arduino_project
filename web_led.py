9080a189a624a05d1718b2ce58c7bbc8daadb5c4
horenkomihailo <horenkomihailo2022@gmail.com>
2025-06-05 19:24:06 +0200

    + update LED project

--- web_led.py
+++ web_led.py
@@ -1,12 +1,35 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, abort
 import serial
+import json
+import os
 
-# Укажи правильный порт для Arduino
+# Загрузка разрешённых IP
+with open("config.json") as f:
+    config = json.load(f)
+
+ALLOWED_IPS = config["allowed_ips"]
+
+# Подключение к Arduino
 arduino = serial.Serial('/dev/ttyACM0', 9600)
 
+# Flask-приложение
 app = Flask(__name__)
 
-@app.route("/", methods=["POST"])
+# Проверка IP
+@app.before_request
+def limit_remote_addr():
+    ip = request.remote_addr
+    if ip not in ALLOWED_IPS:
+        print(f"🚫 Блокировка доступа: {ip}")
+        abort(403)
+
+# Главная страница с кнопками
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("lamp.html")
+
+# POST-запрос от кнопок
+@app.route("/set", methods=["POST"])
 def control_led():
     state = request.form.get("state")
     if state in ["0", "1"]:
@@ -14,5 +37,6 @@ def control_led():
         return f"LED set to {state}", 200
     return "Invalid value", 400
 
+# Запуск
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
